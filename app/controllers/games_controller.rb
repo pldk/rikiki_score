fa760b0b9ec5797c84b392b3bc77f7c16def764a
philippe de liedekerke <philippe.deliedekerke@gmail.com>
2023-05-05 15:29:31 +0200

    eediting game migrations

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -24,6 +24,6 @@ class GamesController < ApplicationController
   private
 
   def game_params
-    params.require(:game).permit(:status, :style)
+    params.require(:game).permit(:status, :style, player_ids: [])
   end
 end
